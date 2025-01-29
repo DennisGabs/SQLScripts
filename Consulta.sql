3ad91118612e481ae43dc3e6de50415430fe9bbe
DENNIS GABRIEL SILVA ALVES <20232ADS.CNT0013@ifma.edu>
2025-01-28 21:22:55 -0300

    add new commands

--- Consulta.sql
+++ Consulta.sql
@@ -7,13 +7,31 @@
 -- USE mysql;
 -- SHOW TABLES;
 
-CREATE DATABASE IF NOT EXISTS aula;
-DROP TABLE IF EXISTS pessoa; 
-SHOW DATABASES;
 USE aula;
-CREATE TABLE Pessoa(
+-- CREATE DATABASE IF NOT EXISTS aula;
+-- DROP TABLE IF EXISTS Pessoa;
+
+SHOW DATABASES;
+CREATE TABLE IF NOT EXISTS Pessoa(
 	cpf CHAR(11) PRIMARY KEY,
-	nome VARCHAR(100)
+	nome VARCHAR(100),
+	CONSTRAINT pk_tbPessoa PRIMARY KEY (cpf)
 );
 
-SHOW TABLES;
+-- INSERT INTO Pessoa(nome, cpf) VALUES ('Dennis Gabriel', '07787337001');
+-- INSERT INTO Pessoa(nome, cpf) VALUES ('Luis XI', '07787337002');
+SELECT * FROM Pessoa;
+
+
+CREATE TABLE IF NOT EXISTS tbCliente(
+	codigo INT,
+    CPF CHAR(11),
+    nome VARCHAR(20) NOT NULL,
+    data_cadastro DATE,
+    cidade VARCHAR(20),
+    UF CHAR(2) DEFAULT 'MA',
+    CONSTRAINT un_CPF UNIQUE (CPF),
+    CONSTRAINT pk_tbCliente PRIMARY KEY(codigo)
+);
+
+SHOW COLUMNS FROM tbCliente;
