c411824f86fd60e36fa87d7041c965186a794985
DENNIS GABRIEL SILVA ALVES <20232ADS.CNT0013@ifma.edu>
2025-02-05 21:26:42 -0300

    other examples

--- consulta_paises.sql
+++ consulta_paises.sql
@@ -15,3 +15,10 @@ SELECT * FROM paises WHERE país LIKE '%a%';
 SELECT * FROM paises WHERE país LIKE '%na%';
 
 SELECT * FROM paises WHERE país LIKE 'a________';
+
+SELECT * FROM paises WHERE país IN ('Argentina', 'Brasil', 'México');
+
+SELECT * FROM paises WHERE país NOT IN ('Argentina', 'Brasil', 'México');
+
+
+SELECT * FROM paises WHERE posição NOT IN (1, 3, 5);
