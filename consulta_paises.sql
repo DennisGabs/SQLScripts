8911749bc35fb040b0eecd3371d05efbc045ac27
DENNIS GABRIEL SILVA ALVES <20232ADS.CNT0013@ifma.edu>
2025-02-05 21:10:11 -0300

    add new examples into the queries

--- consulta_paises.sql
+++ consulta_paises.sql
@@ -5,3 +5,13 @@ SELECT * FROM paises WHERE População > 1000;
 SELECT * FROM paises WHERE País = "CHINA";
 
 SELECT * FROM paises WHERE pib > 10;
+
+SELECT * FROM paises WHERE país LIKE 'a%' ORDER BY PIB DESC;
+
+SELECT * FROM paises WHERE país LIKE '%a';
+
+SELECT * FROM paises WHERE país LIKE '%a%';
+
+SELECT * FROM paises WHERE país LIKE '%na%';
+
+SELECT * FROM paises WHERE país LIKE 'a________';
